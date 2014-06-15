357adb69c569a4d2ce8b25716a8ab1d4684360d5
Zizheng Wu <me@zizhengwu.com>
2014-06-16 01:56:47 +0800

    with images now, cgi enabled

--- amazon.py
+++ amazon.py
@@ -1,28 +1,34 @@
 #!C:/Python27/python
 #-*-coding:utf-8-*-
 from os import *
-from cgi import *
+import cgi
 from bs4 import BeautifulSoup
 import urllib2
 import json
 import re
 
 class Amazon():
-    def main(self, url="http://www.amazon.cn/s/ref=nb_sb_noss_1?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&url=search-alias%3Daps&field-keywords=键盘"):
+    def main(self):
+        fs = cgi.FieldStorage()
         names = []
         links = []
         prices = []
+        srcs = []
         data = []
+        url = "http://www.amazon.cn/s/ref=nb_sb_noss_1?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&url=search-alias%3Daps&field-keywords="+fs["keyword"].value
         page = urllib2.urlopen(url)
         soup = BeautifulSoup(page)
         for i in soup.find_all(lambda tag: tag.name == "div" and tag.get("class") == ['productTitle']):
-            links.append(i.find("a")["href"].strip());
-            names.append(i.get_text().strip());
+            links.append(i.find("a")["href"].strip())
+            names.append(i.get_text().strip())
         for i in soup.find_all("div", "newPrice"):
-            prices.append(i.find("span").get_text().strip());
+            prices.append(i.find("span").get_text().strip())
+        for i in soup.find_all("img", attrs={"alt" : "产品详细信息"}):
+            srcs.append(i["src"])
         data.append(names)
         data.append(links)
         data.append(prices)
+        data.append(srcs)
         print "Content-type: application/json\n"
         print json.dumps(data)
 amazon = Amazon()
--- taobao.py
+++ taobao.py
@@ -1,18 +1,21 @@
 #!C:/Python27/python
 #-*-coding:utf-8-*-
 from os import *  
-from cgi import *
+import cgi
 from bs4 import BeautifulSoup
 import urllib2
 import json
 import re
 
 class Taobao():
-    def main(self, url="http://s.taobao.com/search?q=鼠标"):
+    def main(self):
+        fs = cgi.FieldStorage()
         names = []
         links = []
         prices = []
+        srcs = []
         data = []
+        url = "http://s.taobao.com/search?q="+fs["keyword"].value
         page = urllib2.urlopen(url)
         soup = BeautifulSoup(page)
         for i in soup.select("h3 a"):
@@ -20,9 +23,12 @@ class Taobao():
             names.append(i["title"])
         for i in soup.find_all("div", "price"):
             prices.append(i.get_text().strip())
+        for i in soup.findAll(attrs={"data-ks-lazyload" : re.compile(".*")}):
+            srcs.append(i["data-ks-lazyload"])
         data.append(names)
         data.append(links)
         data.append(prices)
+        data.append(srcs)
         print "Content-type: application/json\n"
         print json.dumps(data)
 taobao = Taobao()
